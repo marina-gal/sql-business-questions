490aeb9871204763b7b20fd8f00883389f392f40
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-11-24 20:24:51 +0100

    Update query28.sql

--- Q28_customers_placed_orders_every_month_in_2011/query28.sql
+++ Q28_customers_placed_orders_every_month_in_2011/query28.sql
@@ -1,1 +1,10 @@
+-- Which customers placed orders every month in 2011 ?
+SELECT 
+     [CustomerID]
+     
+FROM [AdventureWorks2019].[Sales].[SalesOrderHeader]
+WHERE YEAR(OrderDate) = 2011
+GROUP BY CustomerID  -- a row per customer
+	HAVING COUNT(
+			DISTINCT MONTH(OrderDate))= 12 --for each customer we COUNT the different months have in 2011
 
