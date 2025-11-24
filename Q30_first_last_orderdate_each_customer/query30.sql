b6518ae622e605171c6cc23634f69c7e0558316b
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-11-24 19:35:34 +0100

    Update query30.sql

--- Q30_first_last_orderdate_each_customer/query30.sql
+++ Q30_first_last_orderdate_each_customer/query30.sql
@@ -1,1 +1,12 @@
-
+-- Find the first and last order date for each customer
+SELECT 	   
+	    sc.[CustomerID]
+	   ,CONCAT(pp.FirstName, ' ', pp.LastName) AS Customer_Name
+	   ,MIN(soh.OrderDate) AS First_Order
+	   ,MAX(soh.OrderDate) AS Last_Order	   
+           
+FROM [AdventureWorks2019].[Sales].[SalesOrderHeader] AS soh
+	LEFT JOIN Sales.Customer AS sc ON sc.CustomerID = soh.CustomerID
+	LEFT JOIN Person.Person AS pp ON pp.BusinessEntityID = sc.PersonID	
+GROUP BY sc.CustomerID, pp.FirstName, pp.LastName
+ORDER BY Last_Order DESC;
