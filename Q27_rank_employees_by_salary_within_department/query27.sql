bedaf8f3a32780607f1187f5684dc21307cf71f4
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-11-25 15:01:09 +0100

    Update query27.sql

--- Q27_rank_employees_by_salary_within_department/query27.sql
+++ Q27_rank_employees_by_salary_within_department/query27.sql
@@ -1,30 +1,26 @@
 -- Rank employees by salary within the department
-WITH LatestPay AS (
-    SELECT 
-        eph.BusinessEntityID,
-        eph.Rate,
-        ROW_NUMBER() OVER (
-            PARTITION BY eph.BusinessEntityID
-            ORDER BY eph.RateChangeDate DESC
-        ) AS rn
-    FROM HumanResources.EmployeePayHistory AS eph
-)
-SELECT 
-    d.Name AS Department_Name,
-    CONCAT(p.FirstName, ' ', p.LastName) AS Employee_Name,
-    lp.Rate AS Hourly_Rate,
-    lp.Rate * 40 * 52 AS EstimatedAnnualSalary,
-    RANK() OVER (
-        PARTITION BY d.DepartmentID
-        ORDER BY lp.Rate * 40 * 52 DESC
-    ) AS SalaryRank_In_Department
+with LatestPay AS(
+select
+		eph.BusinessEntityID,
+		eph.Rate,
+		ROW_NUMBER()OVER(
+						PARTITION BY eph.BusinessEntityID
+						ORDER BY eph.RateChangeDate DESC) AS Rank_LastSalary
+from HumanResources.EmployeePayHistory AS eph )-- This subquery ranks from the latest salary to the oldest by employee
+
+SELECT
+		CONCAT(p.FirstName, ' ', p.LastName) AS Employee_Name,
+		d.Name AS Department_Name,
+		lp.Rate AS Hourly_Rate,
+		lp.Rate*40*52 AS EstimatedAnnualSalary,
+		
+		RANK() OVER (
+					PARTITION BY d.DepartmentID -- defines groups where to apply ranking
+					ORDER BY lp.Rate*40*52 DESC) AS Salary_Rank_In_Department
+
 FROM LatestPay AS lp
-JOIN HumanResources.EmployeeDepartmentHistory AS edh
-    ON edh.BusinessEntityID = lp.BusinessEntityID
-JOIN HumanResources.Department AS d
-    ON d.DepartmentID = edh.DepartmentID
-JOIN Person.Person AS p
-    ON p.BusinessEntityID = lp.BusinessEntityID
-WHERE lp.rn = 1              -- solo el salario más reciente de cada empleado
-  AND edh.EndDate IS NULL     -- solo su departamento actual
-ORDER BY d.Name, SalaryRank_In_Department;
+	JOIN HumanResources.EmployeeDepartmentHistory AS edh ON edh.BusinessEntityID = lp.BusinessEntityID
+	JOIN HumanResources.Department AS d ON d.DepartmentID = edh.DepartmentID
+	JOIN Person.Person AS p ON p.BusinessEntityID = lp.BusinessEntityID
+WHERE lp.Rank_LastSalary = 1 AND edh.EndDate IS NULL -- the current department 
+ORDER BY d.Name, Salary_Rank_In_Department; -- First, orders by department all, then inside each department orders by ranking
