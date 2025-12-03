df279baadea6b2a88cdd0d209e6f749781f5d69a
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-12-03 18:31:22 +0100

    Update query25.sql

--- Q25_find_all_employees_hired_on_weekends/query25.sql
+++ Q25_find_all_employees_hired_on_weekends/query25.sql
@@ -1,1 +1,12 @@
-
+-- Which customers whichr orders are above average order value
+select   soh.CustomerID,
+		soh.TotalDue,
+		concat(pp.FirstName, ' ', pp.LastName) as Customer_Name
+from [AdventureWorks2019].[Sales].[SalesOrderHeader] as soh
+	left join AdventureWorks2019.Sales.Customer as sc on sc.CustomerID = soh.CustomerID
+	left join AdventureWorks2019.Person.Person as pp on pp.BusinessEntityID = sc.PersonID
+	
+where TotalDue > ( select
+				         AVG(TotalDue)
+				         from [AdventureWorks2019].[Sales].[SalesOrderHeader])
+order by TotalDue DESC;
