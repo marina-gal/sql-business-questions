a0a1c7e06095ac83087b52271e544b403c7cab60
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-12-03 19:06:27 +0100

    Update query32.sql

--- Q32_monthly_sales_revenue_&_order_count/query32.sql
+++ Q32_monthly_sales_revenue_&_order_count/query32.sql
@@ -1,1 +1,7 @@
-
+-- Calculate the monthly sales revenue and order count
+select DATENAME(YEAR,OrderDate) as [Year],
+	   DATENAME(MONTH,OrderDate) as Month_Name,
+	   COUNT(SalesOrderID) as Nb_orders
+from AdventureWorks2019.Sales.SalesOrderHeader
+group by DATENAME(YEAR,OrderDate), DATENAME(MONTH,OrderDate)
+order by DATENAME(YEAR,OrderDate), DATENAME(MONTH,OrderDate), Nb_orders DESC;
