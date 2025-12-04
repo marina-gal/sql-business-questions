c6a71a65c1019ed124dd609289605b26ea22737a
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-12-04 16:45:05 +0100

    Update query34.sql

--- Q34_product_sales_distribution/query34.sql
+++ Q34_product_sales_distribution/query34.sql
@@ -1,1 +1,21 @@
 
+-- Calculate the product sales distribution (percent of total revenue)
+-- first we calculate subquery of the Totalrevenue
+with TotalRevenue as (
+select 
+      SUM(sod.[LineTotal]) as Total_revenue
+from [Sales].[SalesOrderDetail] as sod)
+
+
+-- Then, we CROSS JOIN the subquery to the main query, each of the values
+SELECT
+		p.ProductID,
+		p.Name as ProductName,
+		SUM(sod.LineTotal) as ProductRevenue,
+		SUM(sod.LineTotal)*100.0/tr.Total_revenue as RevenuePercent
+
+FROM Sales.SalesOrderDetail as sod 
+	left join Production.Product as p on p.ProductID = sod.ProductID
+	CROSS JOIN TotalRevenue as tr
+GROUP BY p.ProductID, p.Name, tr.Total_revenue
+ORDER BY RevenuePercent DESC;
