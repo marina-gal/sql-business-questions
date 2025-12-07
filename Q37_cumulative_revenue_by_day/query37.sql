511c2a3d6b478d4b6efd445676eccd895aaf592c
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-12-07 13:53:46 +0100

    Update query37.sql

--- Q37_cumulative_revenue_by_day/query37.sql
+++ Q37_cumulative_revenue_by_day/query37.sql
@@ -1,1 +1,19 @@
+-- Calculate the cumulative amount revenue by day
+-- First, we calculate the daily revenue + group by OrderDate
+with DailyRevenue as(
+select 
+      CAST (soh.OrderDate as date) as OrderDay,  
+      SUM(sod.LineTotal) as DailyRevenue
+     
+from [AdventureWorks2019].[Sales].[SalesOrderHeader] as soh
+inner join Sales.SalesOrderDetail as sod ON sod.SalesOrderID = soh.SalesOrderID
+group by OrderDate)
 
+--Then, the rows from the cummulative revenue + next daily revenue
+SELECT
+		OrderDay,
+		DailyRevenue,
+		SUM(DailyRevenue)OVER(ORDER BY OrderDay
+							  ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as Cumulative_revenue_per_day
+FROM DailyRevenue
+ORDER BY OrderDay;
