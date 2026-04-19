8ab181d0b55409505c9d344ed7aa828193d73d54
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2026-04-19 19:20:07 +0200

    Update query41.sql

--- Q41_percentage_employees_eachdepartment/query41.sql
+++ Q41_percentage_employees_eachdepartment/query41.sql
@@ -1,1 +1,13 @@
-
+-- Percentage of employees of each department
+select  
+d.Name as Department_Name,
+CONCAT(ROUND(COUNT(*)*100.0 /(
+select count(*) 
+from HumanResources.EmployeeDepartmentHistory
+where EndDate IS NULL),2), + ' %')as Percentage_dept
+	
+from HumanResources.EmployeeDepartmentHistory as edh
+inner join HumanResources.Department as d on d.DepartmentID = edh.DepartmentID
+where edh.EndDate IS NULL
+group by d.Name
+order by Percentage_dept DESC
